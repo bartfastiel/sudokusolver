13bd26ad7ee969a7b9b2af54f9665e8c9cc920e1
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-02-26 19:20:25 +0100

    Use shorts with bitmasks instead of boolean arrays
    Performance: 502ms on average
    on Macbook Pro M1 2020 16GB

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -8,15 +8,15 @@ public class SudokuSolver {
     private static final int NUMBER_OF_FIELDS = SIDE_LENGTH * SIDE_LENGTH;
     private final int[] grid;
     private final boolean[] guess = new boolean[NUMBER_OF_FIELDS];
-    private final boolean[][] usedNumbersPerRow = new boolean[SIDE_LENGTH][SIDE_LENGTH];
-    private final boolean[][] usedNumbersPerColumn = new boolean[SIDE_LENGTH][SIDE_LENGTH];
-    private final boolean[][] usedNumbersPerBlock = new boolean[SIDE_LENGTH][SIDE_LENGTH];
+    private final short[] usedNumbersPerRow = new short[SIDE_LENGTH];
+    private final short[] usedNumbersPerColumn = new short[SIDE_LENGTH];
+    private final short[] usedNumbersPerBlock = new short[SIDE_LENGTH];
 
     public SudokuSolver(int[][] grid) {
         if (grid.length != SIDE_LENGTH)
             throw new IllegalArgumentException("Sudoku has to have " + SIDE_LENGTH + " rows but had " + grid.length + " rows");
         if (Arrays.stream(grid).anyMatch(row -> row.length != SIDE_LENGTH))
-            throw new IllegalArgumentException("Each row has to have "+SIDE_LENGTH +" cells");
+            throw new IllegalArgumentException("Each row has to have " + SIDE_LENGTH + " cells");
         if (Arrays.stream(grid).flatMapToInt(Arrays::stream).anyMatch(cell -> cell < 0 || 9 < cell))
             throw new IllegalArgumentException("Only numbers [0," + SIDE_LENGTH + "] are valid inputs");
         var numbersGiven = Arrays.stream(grid).flatMapToInt(Arrays::stream).filter(c -> c != 0).count();
@@ -32,15 +32,15 @@ public class SudokuSolver {
             if (v == 0) {
                 guess[p] = true;
             } else {
-                if (usedNumbersPerRow[row][v - 1])
+                if ((usedNumbersPerRow[row] & (1L << v)) != 0)
                     throw new IllegalArgumentException(v + " twice in row " + row);
-                if (usedNumbersPerColumn[column][v - 1])
+                if ((usedNumbersPerColumn[column] & (1L << v)) != 0)
                     throw new IllegalArgumentException(v + " twice in column " + column);
-                if (usedNumbersPerBlock[block][v - 1])
+                if ((usedNumbersPerBlock[block] & (1L << v)) != 0)
                     throw new IllegalArgumentException(v + " twice in block " + block);
-                usedNumbersPerRow[row][v - 1] = true;
-                usedNumbersPerColumn[column][v - 1] = true;
-                usedNumbersPerBlock[block][v - 1] = true;
+                usedNumbersPerRow[row] |= 1 << v;
+                usedNumbersPerColumn[column] |= 1 << v;
+                usedNumbersPerBlock[block] |= 1 << v;
             }
         }
     }
@@ -65,21 +65,21 @@ public class SudokuSolver {
             var column = p % SIDE_LENGTH;
             var block = (row / 3) * 3 + column / 3;
             if (guess[p]) {
-                var i = grid[p] - 1;
-                if (0 <= i) {
-                    usedNumbersPerRow[row][i] = false;
-                    usedNumbersPerColumn[column][i] = false;
-                    usedNumbersPerBlock[block][i] = false;
+                var v = grid[p];
+                if (1 <= v) {
+                    usedNumbersPerRow[row] &= ~(1 << v);
+                    usedNumbersPerColumn[column] &= ~(1 << v);
+                    usedNumbersPerBlock[block] &= ~(1 << v);
                 }
-                while (i < 8) {
-                    i++;
-                    if (usedNumbersPerRow[row][i]) continue;
-                    if (usedNumbersPerColumn[column][i]) continue;
-                    if (usedNumbersPerBlock[block][i]) continue;
-                    grid[p] = i + 1;
-                    usedNumbersPerRow[row][i] = true;
-                    usedNumbersPerColumn[column][i] = true;
-                    usedNumbersPerBlock[block][i] = true;
+                while (v < 9) {
+                    v++;
+                    if ((usedNumbersPerRow[row] & (1L << v)) != 0) continue;
+                    if ((usedNumbersPerColumn[column] & (1L << v)) != 0) continue;
+                    if ((usedNumbersPerBlock[block] & (1L << v)) != 0) continue;
+                    grid[p] = v;
+                    usedNumbersPerRow[row] |= 1 << v;
+                    usedNumbersPerColumn[column] |= 1 << v;
+                    usedNumbersPerBlock[block] |= 1 << v;
                     direction = 1;
                     continue fieldLoop;
                 }
